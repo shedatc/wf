de92471c189434d098eac19c94aa128b0d9ec1d3
Sheda <sheda@fsfe.org>
2024-03-08 19:48:29 +0100

    Rework arena-0

---
+++ assets/tilemaps/arena-0/tileset-0.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tileset-0" tilewidth="8" tileheight="8" tilecount="1024" columns="32">
+ <image source="tileset-0.png" width="256" height="256"/>
+ <tile id="4">
+  <properties>
+   <property name="is_obstacle" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="is_obstacle" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
