a2fa6230de2baf7f1ca79416ae80b75aaecce4de
Sheda <sheda@fsfe.org>
2024-03-26 18:29:57 +0100

    Refactor EntityFactory a bit

--- lib/WeaponFactory/EntityFactory.py
+++ lib/WeaponFactory/EntityFactory.py
@@ -1,25 +1,14 @@
-from .EngineClock import EngineClock
-from .Entity      import Entity
-from .utils       import log_ex
+from .Entity import Entity
+from .utils  import log_ex
 
 class EntityFactory:
-
-    _singleton = None
-
-    @classmethod
-    def singleton(cls):
-        if cls._singleton is None:
-            cls._singleton = cls()
-        return cls._singleton
 
     @classmethod
     def log(cls, msg):
         log_ex(msg, category=cls.__name__)
 
-    def __init__(self):
-        self._singleton = self
-
-    def spawn(type, position):
+    @classmethod
+    def spawn(cls, type, position):
         EntityFactory.log(f"Spawning entity:")
         EntityFactory.log(f"    Type:     {type}")
         EntityFactory.log(f"    Position: {position}")
