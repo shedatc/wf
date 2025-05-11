529778d59d1d4213bec0abfd00db6c5ccd3d4310
Sheda <sheda@fsfe.org>
2025-05-11 15:12:02 +0200

    cleanup

--- lib/WeaponFactory/Monolith.py
+++
@@ -1,8 +0,0 @@
-from .Entity  import Entity
-from .Physics import Physics
-
-class Monolith(Entity):
-
-    def __init__(self, position):
-        Entity.__init__(self, "monolith-0", position,
-                        speed=0.05)
